cfc85fa40384c2450e3cb079d560c00aca8f60ab
linhr <linhr10@gmail.com>
2014-03-24 20:21:28 +0800

    modify logger

--- ghcrawler/ghcrawler/middlewares/logger.py
+++ ghcrawler/ghcrawler/middlewares/logger.py
@@ -9,11 +9,16 @@ class LoggerMiddleware(object):
 
     def process_response(self, request, response, spider):
         limits = parse_rate_limit(response)
+        
         if self.rate_limit_reset != limits.reset:
             self.rate_limit_reset = limits.reset
-            message = 'Next rate limit reset: %d' % limits.reset
-            log.msg(message, level=log.INFO, spider=spider)
-        message = 'Visited (%d) <%s %s> (rate limit remaining: %d/%d)' % (response.status, 
-            request.method, request.url, limits.remaining, limits.limit)
-        log.msg(message, level=log.INFO, spider=spider)
+            log.msg(format='Next rate limit reset: %(reset)d',
+                level=log.INFO, spider=spider, reset=limits.reset)
+        
+        log.msg(format=('Visited (%(status)d) <%(method)s %(url)s> '
+            '(rate limit remaining: %(remaining)d/%(limit)d)'),
+            level=log.INFO, spider=spider,
+            status=response.status, method=request.method, url=request.url,
+            remaining=limits.remaining, limit=limits.limit)
+        
         return response
