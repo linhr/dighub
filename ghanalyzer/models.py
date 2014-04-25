c39bc70d025e7329de14640156bed10bb7897cda
linhr <linhr10@gmail.com>
2014-04-25 11:15:05 +0800

    add ghanalyzer models

---
+++ ghanalyzer/models.py
@@ -0,0 +1,49 @@
+
+__all__ = [
+    'Account', 'Repository', 'User', 'Organization',
+]
+
+class Entity(object):
+    """base class for hashable entities
+    (can be used as NetworkX nodes)
+    """
+
+    def __init__(self, id):
+        self._id = id
+
+    @property
+    def id(self):
+        return self._id
+
+    def __eq__(self, other):
+        return isinstance(other, self.__class__) and self._id == other._id
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
+
+    def __cmp__(self, other):
+        if not isinstance(other, self.__class__):
+            raise NotImplementedError()
+        return cmp(self._id, other._id)
+
+    def __hash__(self):
+        return self._id
+
+    def __repr__(self):
+        return '%s(%d)' % (type(self).__name__, self._id)
+
+
+class Account(Entity):
+    """account"""
+
+
+class Repository(Entity):
+    """repository"""
+
+
+class User(Account):
+    """user"""
+
+
+class Organization(Account):
+    """organization"""
