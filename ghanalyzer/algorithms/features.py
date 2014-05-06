fa3dba837fbf45a4b14545c108f86fc583ce7814
linhr <linhr10@gmail.com>
2014-05-06 10:43:04 +0800

    add description analysis command

--- ghanalyzer/algorithms/features.py
+++ ghanalyzer/algorithms/features.py
@@ -1,7 +1,11 @@
 import numpy
+from nltk.corpus import stopwords
 from sklearn.feature_extraction import DictVectorizer
-from sklearn.decomposition import PCA
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.decomposition import PCA, NMF
 from sklearn.preprocessing import normalize
+from gensim.models.ldamodel import LdaModel
+from gensim.matutils import Sparse2Corpus
 
 
 class LanguagePCA(object):
@@ -20,3 +24,35 @@ class LanguagePCA(object):
         names = numpy.array(names, dtype=object)
         indices = numpy.argsort(-numpy.absolute(self.transformer.components_))
         return names[indices]
+
+
+class DescriptionLDA(object):
+    def __init__(self, dataset, n_topics):
+        self.dataset = dataset
+        self.n_topics = n_topics
+        self.vectorizer = CountVectorizer(stop_words=stopwords.words('english'))
+        self.samples = dataset.keys()
+        self.features = self.vectorizer.fit_transform(dataset.values())
+        
+        id2word = {i: w for w, i in self.vectorizer.vocabulary_.iteritems()}
+        corpus = Sparse2Corpus(self.features, documents_columns=False)
+        self.transformer = LdaModel(corpus=corpus, id2word=id2word, num_topics=n_topics)
+
+
+class DescriptionNMF(object):
+    def __init__(self, dataset, n_topics=None, vocabulary_size=None):
+        self.dataset = dataset
+        self.n_topics = n_topics
+        self.vectorizer = CountVectorizer(max_features=vocabulary_size,
+            stop_words=stopwords.words('english'))
+        self.samples = dataset.keys()
+        self.features = self.vectorizer.fit_transform(dataset.values())
+        self.features = TfidfTransformer().fit_transform(self.features)
+        self.transformer = NMF(n_components=n_topics)
+        self.transformer.fit(self.features)
+        
+    def get_components(self):
+        words = self.vectorizer.get_feature_names()
+        words = numpy.array(words, dtype=object)
+        indices = numpy.argsort(-numpy.absolute(self.transformer.components_))
+        return words[indices]
---
+++ ghanalyzer/commands/descriptions.py
@@ -0,0 +1,27 @@
+
+from ghanalyzer.command import AnalyzerCommand
+from ghanalyzer.algorithms.features import DescriptionLDA, DescriptionNMF
+from ghanalyzer.io import load_repository_descriptions
+
+class Command(AnalyzerCommand):
+    def description(self):
+        return 'analyze repository descriptions'
+
+    def define_arguments(self, parser):
+        parser.add_argument('type', choices=['lda', 'nmf'])
+        parser.add_argument('-d', '--data-path', required=True)
+        parser.add_argument('--topic-count', type=int, default=10)
+        parser.add_argument('--vocabulary-size', type=int, default=None)
+        parser.add_argument('--show-fork', action='store_true')
+
+    def run(self, args):
+        descriptions = load_repository_descriptions(args.data_path,
+            show_fork=bool(args.show_fork))
+
+        if args.type == 'lda':
+            analyzer = DescriptionLDA(descriptions, n_topics=args.topic_count)
+        elif args.type == 'nmf':
+            analyzer = DescriptionNMF(descriptions, n_topics=args.topic_count,
+                vocabulary_size=args.vocabulary_size)
+
+        return {'descriptions': descriptions, 'analyzer': analyzer}
