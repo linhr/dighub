73449a26f78533c5af90720c7ee558d279688898
linhr <linhr10@gmail.com>
2014-05-29 08:20:23 +0800

    speed up frequency calculation

--- ghanalyzer/evaluation/metrics.py
+++ ghanalyzer/evaluation/metrics.py
@@ -5,25 +5,34 @@ from scipy.stats import nanmean
 
 
 def get_frequencies(report, cutoff=None):
-    repos = set(r.id for r in report['repos'])
+    repo_map = {r.id: i for i, r in enumerate(report['repos'])}
+    repo_count = len(repo_map)
     case_count = len(report['recommendation'])
     try:
         cutoff = tuple(cutoff)
     except TypeError:  # cutoff is a single value (None or int)
         cutoff = (cutoff,)
+
+    def _get_indices(repos):
+        return [repo_map[r.id] for r in repos]
+
     frequencies = np.empty((case_count, len(cutoff), 4))
     for i, x in enumerate(report['recommendation']):
-        candidates = repos - set(r.id for r in x['training'])
-        groundtruth = set(r.id for r in x['groundtruth'])
-        recommended = [r.id for r in x['recommended']]
+        candidates = np.ones(repo_count, dtype=bool)
+        candidates[_get_indices(x['training'])] = False
+        groundtruth = np.zeros(repo_count, dtype=bool)
+        groundtruth[_get_indices(x['groundtruth'])] = True
+        recommended = np.zeros(repo_count, dtype=int)
+        recommended[_get_indices(x['recommended'])] = np.arange(len(x['recommended'])) + 1
         for j, k in enumerate(cutoff):
-            # recommendation list is not sliced if k is None
-            positive = set(recommended[:k])
-            negative = candidates - positive
-            tp = len(positive & groundtruth)
-            fp = len(positive) - tp
-            fn = len(negative & groundtruth)
-            tn = len(negative) - fn
+            positive = recommended > 0
+            if k is not None:
+                positive &= recommended <= k
+            negative = candidates & (~positive)
+            tp = np.count_nonzero(positive & groundtruth)
+            fp = np.count_nonzero(positive) - tp
+            fn = np.count_nonzero(negative & groundtruth)
+            tn = np.count_nonzero(negative) - fn
             frequencies[i, j, :] = tp, tn, fp, fn
     if len(cutoff) == 1:
         frequencies = np.reshape(frequencies, (case_count, 4))
